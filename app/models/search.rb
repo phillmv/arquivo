b002ef8c2b5b5882fd054ce9015f453b234dbfa7
Phill MV <phillmv@okayfail.com>
2020-05-05 14:46:03 -0400

    halfassed a better search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,24 @@
 class Search
   def self.find(notebook:, query:, page: nil)
-    Entry.for_notebook(notebook).where("body like ?", "%#{query}%").order(occurred_at: :desc)
+    tokens = []
+
+    # super lazy quick way of doing this
+    query.gsub(/"([^"]*)"/) do |match|
+      tokens << match.gsub('"', '')
+      ""
+    end.split do |s|
+      tokens << s
+    end
+
+    sql_where = tokens.map do |s|
+      ["body like ? or subject like ?", "%#{s}%", "%#{s}%"]
+    end
+
+    sql_query = Entry.for_notebook(notebook).order(occurred_at: :desc)
+    sql_where.each do |where|
+      sql_query = sql_query.where(*where)
+    end
+
+    sql_query
   end
 end
