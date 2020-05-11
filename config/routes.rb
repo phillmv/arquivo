e8ae683a28c0f50882918ef66f67775abc2cd78c
Phill MV <phillmv@okayfail.com>
2020-05-11 12:23:38 -0400

    removed superfluous /entries/ from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :notebooks
 
   scope ':notebook', defaults: { notebook: "journal" } do
-    resources :entries
     get '/timeline', to: "timeline#index", as: :timeline
     get '/timeline/search', to: "timeline#search", as: :search
     get '/calendar', to: "calendar#monthly", as: :calendar
     get '/calendar/:date', to: "calendar#daily", as: :calendar_daily
+
+
+    resources :entries, path: "/"
 
     get "/settings", to: "settings#index", as: :settings
     post "/settings/add_calendar", to: "settings#add_calendar", as: :add_calendar
