40bf125c1cbd20332a477bf7200bd98d676f80a9
Phill MV <phillmv@okayfail.com>
2020-07-05 10:34:32 -0400

    added quote saved searches unquote tweaked tabnav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  TABNAV_OPTS = {
+    "timeline/index" => :timeline,
+    "timeline/agenda" => :this_day,
+    "calendar/weekly" => :this_week,
+    "calendar/monthly" => :this_month,
+  }
+  def current_tabnav(opt)
+    if TABNAV_OPTS[current_action] == opt
+      "aria-current='page'"
+    else
+      nil
+    end
+  end
+
+  def current_action
+    @current_action ||= "#{controller_name}/#{action_name}"
+  end
 end
