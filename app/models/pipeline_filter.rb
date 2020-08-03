d268b1aa50f2dd856a25b0ba19cd10a36a6104ed
Phill MV <phillmv@okayfail.com>
2020-08-03 13:08:13 -0400

    WIP for making todo mode work

--- app/models/pipeline_filter.rb
+++ app/models/pipeline_filter.rb
@@ -6,4 +6,45 @@ module PipelineFilter
   ESW[:attributes] = ESW[:attributes].dup
   ESW[:attributes][:all] = ESW[:attributes][:all].dup
   ESW[:attributes][:all].push("data-sourcepos")
+
+  TODO_SANITIZATION_WHITELIST = ENTRY_SANITIZATION_WHITELIST.dup
+  TSW = TODO_SANITIZATION_WHITELIST
+
+  # The Todo render pipeline is the same as the Entry pipeline, but we run
+  # a second sanitization step after the TaskFilter. There is a 2-step trick here:
+  #
+  # 1. Because Commonmarker wraps everything in a <p> tag, by exclusing the p tag
+  # from the allowed elements, we delete virtually all html that is not in a list
+  #
+  # 2. We attach a transformer lambda that checks list tags to see if they have
+  # been given the task list class.
+  TSW[:elements] = TSW[:elements] - %w[p blockquote h1 h2 h3 h4 h5 h6 hr pre]
+  TSW[:remove_contents] = %w[p pre h1 h2 h3 h4 h5 h6]
+
+  # this is why we have to run the sanitization filter twice:
+  # the TaskFilter introduces input fields, which would be otherwise removed.
+  # Because we've already sanitized this once, we can be assured this
+  # whitelisted input actually came from the TaskFilter
+  TSW[:elements] = TSW[:elements] + %w[input]
+  TSW[:attributes] = TSW[:attributes].dup
+  TSW[:attributes]['input'] = ['class']
+  TSW[:attributes]['ul'] = ['class']
+  TSW[:attributes]['ol'] = ['class']
+  TSW[:attributes]['li'] = ['class']
+  TSW[:transformers] = [
+    lambda do |env|
+      name = env[:node_name]
+      node = env[:node]
+
+      if ["ul","ol"].include?(name)
+        if node.attributes["class"].present? && node.attributes["class"].value == "task-list"
+        else
+          node.remove
+        end
+      end
+    end
+  ].freeze
+
+  # TODO: handle p nested inside a list, handle lis without the task list class. might have to make a lambda for all of this
+
 end
---
+++ app/models/todo_renderer.rb
@@ -0,0 +1,37 @@
+require 'task_list/filter'
+class TodoRenderer
+  attr_accessor :entry
+  def initialize(entry)
+    @entry = entry
+  end
+
+  PIPELINE = HTML::Pipeline.new [
+    PipelineFilter::MarkdownFilter,
+    HTML::Pipeline::SanitizationFilter,
+    TaskList::Filter,
+    PipelineFilter::DumbSanitizationFilter,
+    PipelineFilter::HashtagFilter,
+    PipelineFilter::MentionFilter,
+    HTML::Pipeline::TableOfContentsFilter,
+    HTML::Pipeline::ImageMaxWidthFilter,
+  ], { unsafe: true,
+       whitelist: PipelineFilter::ENTRY_SANITIZATION_WHITELIST,
+       second_whitelist: PipelineFilter::TODO_SANITIZATION_WHITELIST,
+       commonmarker_render_options: [:SOURCEPOS]
+  }
+
+  # TODO: to_html should accept… a string, right?
+  # more importantly: cache the results, so that we may
+  # filter out empty entries
+
+  def to_html(attribute_name = "body")
+    attribute = entry.attributes[attribute_name]
+    attribute ||= entry.send(attribute_name) if attribute_name == "todo_body"
+    if !attribute
+      ""
+    else
+      PIPELINE.to_html(attribute, entry: entry).html_safe
+    end
+
+  end
+end
