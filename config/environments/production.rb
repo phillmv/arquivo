13de3c5326a34fd6b811f1f7472770c2400da8b2
Phill MV <phillmv@okayfail.com>
2021-03-28 14:15:46 -0400

    messing with assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # until we figure out how to multitenant this,
   config.active_storage.routes_prefix = "/#{User.current}/_"
+  config.assets.prefix = "/#{User.current}/_"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
