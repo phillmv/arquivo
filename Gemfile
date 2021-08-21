6e8e37e26061753fd2a834a09b94ff7fb500fb14
Phill MV <phillmv@okayfail.com>
2021-08-21 11:06:28 -0400

    Updated commonmarker, and with it I no longer need the footnotes options hack.

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
-gem "commonmarker"
+gem "commonmarker", git: 'https://github.com/phillmv/commonmarker.git', branch: 'footnotes-patch'
 gem "octicons"
 gem "octicons_helper"
 gem "html-pipeline"
--- app/models/pipeline_filter/markdown_filter.rb
+++ app/models/pipeline_filter/markdown_filter.rb
@@ -1,25 +1,3 @@
-# For reasons I do not comprehend, CommonMarker distinguishes between render
-# and parse options, throwing an error should you pass in a flag enabling a
-# parse bitmask that is not defined as a render option.
-#
-# Also confusingly, it's not obvious how to pass along parse vs render options.
-#
-# However, if you forcibly pass in the FOOTNOTES bitmask into the render
-# options… it just works??
-#
-# I have not yet gone thru the C code to try to understand wtf is going on
-# (and tbh I idk that I'd get this thru as a PR) but in the meantime here is
-# an insanely silly hack that adds the FOOTNOTES bitmask option to the Render
-# Config class, thereby allowing me to pass it along as an option to
-# CommonMarker#render_html.
-module CommonMarker
-  module Config
-    class Render
-      define(:FOOTNOTES, (1 << 13)) unless key?(:FOOTNOTES)
-    end
-  end
-end
-
 # extended from the original solely in order to add the ability to pass in
 # render options via the `commonmarker_render` context flag
 
