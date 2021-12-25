93eab22eea0b1930bc73211d951868df118ee18a
Phill MV <phillmv@okayfail.com>
2021-12-25 15:22:56 -0500

    Moved EntryLinker from regex to html parsing, added filtering.

--- app/models/entry_linker.rb
+++ app/models/entry_linker.rb
@@ -1,24 +1,55 @@
 # should be run after the EntryTagger, since we rely on Entry tags
 class EntryLinker
-  URL_REGEX = Regexp.new("(#{URI.regexp})", true)
-  WIKILINK_REGEX = /\[\[([^\]|]*)(\|([^\]]*))?\]\]/
-  attr_reader :entry
+  attr_reader :entry, :notebook
 
   def initialize(entry)
     @entry = entry
+    @notebook = entry.parent_notebook
   end
 
-  def extract_urls(body)
-    urls = body&.scan(URL_REGEX)&.map(&:first)&.flatten || []
-    wiki_urls = body&.scan(WIKILINK_REGEX)&.map(&:first) || []
+  DocumentFragment = Nokogiri::HTML::DocumentFragment
+  def extract_urls
+    html = EntryRenderer.new(entry).to_html2
+    doc = DocumentFragment.parse(html)
 
-    urls + wiki_urls
+    doc.css("a[href]").reduce([]) do |arr, a|
+      unless reject_url?(a["href"])
+        arr << a["href"]
+      end
+
+      arr
+    end
+  end
+
+  def reject_url?(href)
+    # originally I went with:
+    # if a["href"] =~ /^(http|\/)/
+    # but on 2nd thought, the only kind of "bad" url i wouldn't consider a
+    # "real" link is an anchor to something on the current path, or one of
+    # the blacklisted paths.
+    href[0] == "#" || blacklisted_paths.any? do |path|
+      href.index(path)
+    end
+  end
+
+  def blacklisted_paths
+    if defined?(@blacklisted_paths)
+      return @blacklisted_paths
+    end
+
+    # don't want to consider autogen tag and contact urls to be a "link"
+    # since we keep track of those references separately ¯\_(ツ)_/¯
+    # TODO: rewrite this to handle Arquivo.static urls
+    tag_search_path = Rails.application.routes.url_helpers.search_path(owner: notebook.owner, notebook: notebook, query: "#")
+    contact_search_path = Rails.application.routes.url_helpers.search_path(owner: notebook.owner, notebook: notebook, query: "@")
+
+    @blacklisted_paths = [tag_search_path, contact_search_path]
   end
 
   def link!
     notebook = Notebook.find_by!(name: entry.notebook)
 
-    links = extract_urls(entry.body).map do |url|
+    links = extract_urls.map do |url|
       link = notebook.links.find_by(url: url)
 
       if link.nil?
--- test/models/entry_linker_test.rb
+++ test/models/entry_linker_test.rb
@@ -3,26 +3,51 @@ require 'test_helper'
 class EntryLinkerTest < ActiveSupport::TestCase
   setup do
     @notebook = Notebook.create(name: "test")
+    @linked_entry = @notebook.entries.notes.create(body: "# Hello world!\r\nI am a note!")
   end
 
   test "links are extracted" do
     assert_equal 0, Link.count
-    entry = @notebook.entries.create(body: "#test\n https://example.com\r\n\r\nhttp://okayfail.com")
+    body = <<~ENTRY_BODY
+    #test
 
-    assert_equal 2, Link.count
-    assert_equal 2, entry.links.count
+    https://example.com
+
+    http://okayfail.com
+
+    [[#{@linked_entry.identifier}]]
+
+    @name
+
+    <a href="https://example.com/example">another example</a>
+
+    <a href="#foo">but this will not work</a>
+    ENTRY_BODY
+    entry = @notebook.entries.create(body: body)
+
+    assert_equal 4, Link.count
+    assert_equal 4, entry.links.count
 
     # this operation is idempotent
     EntryLinker.new(entry).link!
 
-    assert_equal 2, Link.count
-    assert_equal 2, entry.links.count
+    assert_equal 4, Link.count
+    assert_equal 4, entry.links.count
 
-    assert_equal entry.links.map(&:url).to_set, ["https://example.com", "http://okayfail.com"].to_set
+    # of note here is that the #test and @name links
+    # are not included as "links", and neither is the anchor #foo
+    expected_links = ["https://example.com",
+                      "http://okayfail.com",
+                      "/#{@notebook.owner}/#{@notebook}/#{@linked_entry.identifier}",
+                      "https://example.com/example"]
+
+    assert_equal entry.links.map(&:url).to_set, expected_links.to_set
 
     entry2 = @notebook.entries.create(body: "#foo #bar\r\nhttps://example.com")
 
-    assert_equal 2, Link.count
+    # since example.com has already been linked, the Link count does not increase
+    assert_equal 4, Link.count
+    assert_equal 5, LinkEntry.count
     assert_equal 1, entry2.links.count
   end
 end
