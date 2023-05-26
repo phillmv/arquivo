b8a4c961b76b06927efba26966aa6bbc790f567e
Phill MV <phillmv@okayfail.com>
2023-05-25 20:52:27 -0400

    Upgraded framework defaults.

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,7 @@ Bundler.require(*Rails.groups)
 module Arquivo
   PERMITTED_YAML = [Symbol, Date, Time, ActiveSupport::HashWithIndifferentAccess, ActiveSupport::TimeWithZone]
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
 
