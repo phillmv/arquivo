b135a28395bff1e8d7bf9cad9fdd1d3fa58066ab
Phill MV <phillmv@okayfail.com>
2023-06-16 13:31:13 -0400

    Allow webconsole to work from inside docker.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require 'socket'
 
 Rails.application.configure do
   config.hosts << "arquivo.io"
@@ -72,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # for use within docker, thanks https://stackoverflow.com/a/58702345
+  config.web_console.permissions = Socket.getifaddrs.select { |ifa| ifa.addr.ipv4_private? }.map { |ifa| IPAddr.new(ifa.addr.ip_address + '/' + ifa.netmask.ip_address) }
 end
