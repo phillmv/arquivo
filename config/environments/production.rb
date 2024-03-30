c452cfac2beec14578234a2bfe40512de1f70973
Phill MV <phillmv@okayfail.com>
2024-03-30 14:23:50 -0400

    silence sqlite warning, i get it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,8 @@ Rails.application.configure do
     config.hosts << "localhost"
   end
 
+  config.active_record.sqlite3_production_warning=false
+
   # until we figure out how to multitenant this,
   config.active_storage.routes_prefix = ENV["ARQUIVO_USER"] || "/phillmv/_"
   # config.active_storage.routes_prefix = "/#{User.current}/_"
